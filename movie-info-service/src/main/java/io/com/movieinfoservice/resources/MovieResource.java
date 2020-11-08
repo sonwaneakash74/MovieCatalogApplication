9a92990aa10f5efcc1c1a06e1a323c6bd456a576
Akash Sonawane <sonawaneakash74@gmail.com>
2020-11-08 14:41:30 +0530

    movie-info-service first commit

---
+++ movie-info-service/src/main/java/io/com/movieinfoservice/resources/MovieResource.java
@@ -0,0 +1,34 @@
+package io.com.movieinfoservice.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.com.movieinfoservice.models.Movie;
+import io.com.movieinfoservice.models.MovieSummary;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+   /* @RequestMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
+        MovieSummary movieSummary = restTemplate.getForObject("https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" +  apiKey, MovieSummary.class);
+        return new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
+
+    }*/
+    
+    @RequestMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
+        return new Movie(movieId, "ABC", "XYZ");
+    }
+  
+
+}
